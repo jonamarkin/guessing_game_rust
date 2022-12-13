cef3805ac3925bd5ce463c73db14753daa1f29a6
Jonathan Ato Markin <jona10711@gmail.com>
2022-12-13 02:43:27 +0100

    added color to texts

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 500);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "too small".red()),
+            Ordering::Greater => println!("{}", "too big".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
